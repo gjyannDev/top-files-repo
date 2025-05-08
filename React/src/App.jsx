7d5ba528240786ed2f330d10ab92aaa9f1caf496
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-08 22:52:12 +0800

    chore: did the second practice file, just added a condition when the checkbox is true

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,45 +1,40 @@
-import { useState, useEffect } from "react";
-import { createConnection } from "./Chat";
-
-const serverUrl = "https://localhost:1234";
-
-function ChatRoom({ roomId }) {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    const connection = createConnection(serverUrl, roomId);
-    connection.connect();
-    return () => connection.disconnect();
-  }, [roomId]);
-
-  return (
-    <>
-      <h1>Welcome to the {roomId} room!</h1>
-      <input
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-    </>
-  );
-}
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [roomId, setRoomId] = useState("general");
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [canMove, setCanMove] = useState(true);
+
+  useEffect(() => {
+    function handleMove(e) {
+      !canMove ? setPosition({ x: e.clientX, y: e.clientY }) : position;
+    }
+    window.addEventListener('pointermove', handleMove);
+    return () => window.removeEventListener('pointermove', handleMove);
+  }, [canMove, position]);
+
+
   return (
     <>
       <label>
-        Choose the chat room:{" "}
-        <select
-          value={roomId}
-          onChange={(e) => setRoomId(e.target.value)}
-        >
-          <option value="general">general</option>
-          <option value="travel">travel</option>
-          <option value="music">music</option>
-        </select>
+        <input type="checkbox"
+          checked={canMove}
+          onChange={e => setCanMove(e.target.checked)} 
+        />
+        The dot is allowed to move
       </label>
       <hr />
-      <ChatRoom roomId={roomId} />
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'pink',
+        borderRadius: '50%',
+        opacity: 0.6,
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+      }} />
     </>
   );
 }

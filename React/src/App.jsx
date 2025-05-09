a7efc03ed2b06cd9111bd7a1c3b611aea4c2ce98
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-09 22:05:54 +0800

    chore: solve problem practice about not using useEffect

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,51 +1,51 @@
-import { useState, useEffect } from 'react';
-import { fetchData } from './api';
+import { useState } from 'react';
+import { initialTodos, createTodo } from './Todos';
 
-export default function Page() {
-  const [planetList, setPlanetList] = useState([])
-  const [planetId, setPlanetId] = useState('');
-
-  const [placeList, setPlaceList] = useState([]);
-  const [placeId, setPlaceId] = useState('');
-
-  useEffect(() => {
-    let ignore = false;
-    fetchData('/planets').then(result => {
-      if (!ignore) {
-        console.log('Fetched a list of planets.');
-        setPlanetList(result);
-        setPlanetId(result[0].id); // Select the first planet
-      }
-    });
-    return () => {
-      ignore = true;
-    }
-  }, []);
+export default function TodoList() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [showActive, setShowActive] = useState(false);
+  const active_todos = todos.filter(todo => !todo.completed);
+  const all_todos = showActive ? active_todos : todos
 
   return (
     <>
       <label>
-        Pick a planet:{' '}
-        <select value={planetId} onChange={e => {
-          setPlanetId(e.target.value);
-        }}>
-          {planetList.map(planet =>
-            <option key={planet.id} value={planet.id}>{planet.name}</option>
-          )}
-        </select>
+        <input
+          type="checkbox"
+          checked={showActive}
+          onChange={e => setShowActive(e.target.checked)}
+        />
+        Show only active todos
       </label>
-      <label>
-        Pick a place:{' '}
-        <select value={placeId} onChange={e => {
-          setPlaceId(e.target.value);
-        }}>
-          {placeList.map(place =>
-            <option key={place.id} value={place.id}>{place.name}</option>
-          )}
-        </select>
-      </label>
-      <hr />
-      <p>You are going to: {placeId || '???'} on {planetId || '???'} </p>
+      <NewTodo onAdd={newTodo => setTodos([...todos, newTodo])} />
+      <ul>
+        {all_todos.map(todo => (
+          <li key={todo.id}>
+            {todo.completed ? <s>{todo.text}</s> : todo.text}
+          </li>
+        ))}
+      </ul>
+      <footer>
+        {active_todos.length} todos left
+      </footer>
+    </>
+  );
+}
+
+function NewTodo({ onAdd }) {
+  const [text, setText] = useState('');
+
+  function handleAddClick() {
+    setText('');
+    onAdd(createTodo(text));
+  }
+
+  return (
+    <>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={handleAddClick}>
+        Add
+      </button>
     </>
   );
 }
